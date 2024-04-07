04bca5a8ceb9be4531d68461000e684d4259111a
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-04-07 11:05:59 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExample : ${prefixe}fancy 10 Kimmy\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('FLASH-MD', fancy));
+            return await repondre(`\nExample : ${prefixe}fancy 10 Kimmy\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('KIMMY', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
