e0e678f3e0e0933b0d369d39eab5c5ad680023f0
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-05-04 21:48:31 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_Uptime of MEGATRON-BOT: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_𝑼𝒑𝒕𝒊𝒎𝒆 𝒐𝒇  𝑴𝑬𝑮𝑨𝑻𝑹𝑶𝑵 𝑩𝑶𝑻: ${runtime(process.uptime())}_*`) 
 
    
 
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: 'Web Screenshot by *MEGATRON-BOT*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 * 𝑴𝑬𝑮𝑨𝑻𝑹𝑶𝑵 𝑩𝑶𝑻*'}, { quoted: ms });
 
 
 }
